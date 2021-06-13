88d10228c226b4353189743c207c8bb09c3cfb07
olde <oldemar414@gmail.com>
2021-06-13 14:26:28 -0600

    Cree la rama showActivity

---
+++ app/src/main/java/com/example/firestore_crud/ShowActivity.java
@@ -0,0 +1,14 @@
+package com.example.firestore_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
