204839c27dbaa9868112454c3fac3656a6c12189
olde <oldemar414@gmail.com>
2021-06-14 12:47:56 -0600

    se realizo el delete

--- app/src/main/java/com/example/firestore_crud/MyAdapter.java
+++ app/src/main/java/com/example/firestore_crud/MyAdapter.java
@@ -6,9 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
@@ -16,7 +21,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     private ShowActivity activity;
     private List<Model> mList;
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public MyAdapter(ShowActivity activity, List<Model> mList) {
         this.activity = activity;
@@ -36,6 +41,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
         activity.startActivity(intent);
     }
 
+    public void deleteData(int position){
+        Model item = mList.get(position);
+        db.collection("Documents").document(item.getId()).delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            notifyRemoved(position);
+                            Toast.makeText(activity, "Data delete", Toast.LENGTH_SHORT).show();
+                        } else{
+                            Toast.makeText(activity, "Error"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void notifyRemoved(int position){
+        mList.remove(position);
+        notifyItemRemoved(position);
+        activity.showData();
+    }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
